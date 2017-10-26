0e2f7dc3e1016a22dfd0f0df93bf47a567b5c9fc
sintasafira <firasapphire@gmail.com>
2017-10-26 17:04:02 +0700

    lavarel berhasil

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('tentang', function () {
+    return view('tentang');
+});
+
+Route::get('percakapan', function () {
+    return view('percakapan');
+});
+
+Route::post('percakapan/kirim', function () {
+    return view('percakapan_kirim');
+});
+
+Route::get('template',function () {
+	return view('coba2');
+});
+
+Route::get('coba1',function () {
+	return view('coba1');
+});
+
+Route::get('coba2',function () {
+	return view('coba2');
+});
+
